fb08502bc9aee0a3885a8ea99616ec277701a4df
Waves <creepykid1101@gmail.com>
2018-03-06 16:33:49 -0600

    Add files via upload

---
+++ pbot.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const settings = require('./settings.json');
+const stuff = require('./stuff.json');
+const bot = new Discord.Client();
+const pfix = ';';
+
+bot.on('ready', () => {
+bot.user.setActivity('Being recoded sorry guys');
+});
+
+bot.on('message', message => {
+let msg = message.content.toUpperCase();
+let user = message.author;
+let args = message.content.slice(pfix.length).trim().split(' ');
+let cmd = args.shift().toLowerCase();
+
+if(!msg.startsWith(pfix)) return;
+if(user.bot) return;
+
+try{
+let commandFile = require(`./commands/${cmd}.js`);
+commandFile.run(bot, message, args);
+} catch(e) {
+console.log(e.message);
+}
+});
+
+bot.login(settings.token)
