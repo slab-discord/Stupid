284c3c6c52d45e2a97349f4b6626d20ae8b1a2bb
Waves <creepykid1101@gmail.com>
2018-03-06 22:21:55 -0600

    Create settings.js

---
+++ commands/settings.js
@@ -0,0 +1,9 @@
+{
+  "token": "my token not 4 u",
+  "mtoken": "token to my almost music bot",
+  "version": "1.1.1.1 Beta",
+  "myid": "363474941523263518",
+  "psvr": "253363579976155138",
+  "passes" : 1,
+  "blacklist": ""
+}
