c65cb2f2e2113229baa160d07fcce7e090fb2ca3
Waves <creepykid1101@gmail.com>
2018-04-09 14:25:26 -0500

    Create ahelp.js

---
+++ commands/ahelp.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const settings = require('./settings.json');
+
+exports.run = (bot, message, args, tools) => {
+if(!settings.ids.includes(message.author.id) return;
+bot.users.get(message.author.id).send(new Discord.RichEmbed()
+.setTitle('Admin Help')
+.setDescription('**Admin Commands**\n\n**;eval** - Eval some code to test\n\nMore coming soon...')
+.setColor('#FFA500'));
+}
