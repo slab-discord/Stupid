193d3a9b55ccb57c15f763a1867c7282c5abbada
Waves <creepykid1101@gmail.com>
2018-08-19 21:13:10 +0000

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -9,7 +9,7 @@ let sinfo = new Discord.RichEmbed()
 .addField('Owner', message.guild.owner.user.tag)
 .addField('Server ID', message.guild.id)
 .addField('Members', message.guild.memberCount)
-.addField('Humans', `${message.guild.members.filter(m => !m.user.bot).size} Total\n${message.guild.members.filter(m =>!m.user.bot && m.presence.status === 'online').size} Online\n${message.guild.members.filter(m => m.presence.status === 'idle').size} Idle\n${message.guild.members.filter(m => m.presence.status === 'dnd').size} Do Not Disturb\n${message.guild.members.filter(m => m.presence.status === 'offline').size} Offline/Invis`)
+.addField('Humans', `${message.guild.members.filter(m => !m.user.bot).size} Total\n${message.guild.members.filter(m =>!m.user.bot && m.presence.status === 'online').size} Online\n${message.guild.members.filter(m => !m.user.bot && m.presence.status === 'idle').size} Idle\n${message.guild.members.filter(m => !m.user.bot && m.presence.status === 'dnd').size} Do Not Disturb\n${message.guild.members.filter(m => !m.user.bot && m.presence.status === 'offline').size} Offline/Invis`)
 .addField('Bots', message.guild.members.filter(m => m.user.bot).size)
 .addField('Roles (' + message.guild.roles.size + ')', message.guild.roles.map(r => r.name).slice(0, 25).join(' | ') + '...')
 .setFooter('Requested by ' + message.author.tag);
