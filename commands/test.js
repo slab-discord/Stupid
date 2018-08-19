56322c686f9c2dfe650c235e16570225fc586afd
Waves <creepykid1101@gmail.com>
2018-08-19 20:31:51 +0000

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -9,7 +9,7 @@ let sinfo = new Discord.RichEmbed()
 .addField('Owner', message.guild.owner.user.tag)
 .addField('Server ID', message.guild.id)
 .addField('Members', message.guild.memberCount)
-.addField('Humans', `${message.guild.members.filter(m => !m.user.bot).size} Total\n${message.guild.members.filter(m => m.author.presence === 'ONLINE').size} Online`)
+.addField('Humans', `${message.guild.members.filter(m => !m.user.bot).size} Total\n${message.guild.members.filter(m => m.presence === 'ONLINE').size} Online`)
 .addField('Bots', message.guild.members.filter(m => m.user.bot).size)
 .addField('Roles (' + message.guild.roles.size + ')', message.guild.roles.map(r => r.name).slice(0, 25).join(' | ') + '...')
 .setFooter('Requested by ' + message.author.tag);
