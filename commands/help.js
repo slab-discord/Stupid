bac0d0dd1558f40a171b2c27b2c1d615486dd438
Waves <creepykid1101@gmail.com>
2018-08-21 21:52:51 +0000

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,7 @@ exports.run = (bot, message, args, tools) => {
 let help = new Discord.RichEmbed()
 .setTitle('Help Page')
 .setColor('#FFA500')
-.setDescription('**Util Commands**\n\n**;serverinfo** - Shows info about the server.\n**;userinfo** - Shows info about you or another person\n**;botinfo** - Info about me\n**;kick** - Kick a user\n\n**Role commands**\n\n**;role** - Custom roles. Role name must start with • and be below the bots role\n**;color** - Colored roles. Role name must start with # and be below the the bots role\n\n**Fun Commands**\n\n**;cat** - Random cat\n**;gay** - Call someone gay\n**;retarded** - Call someone retarded\n**;say** - Make me say stuff\n**;yomama** - Yo mama joke\n**;8ball** - Magic 8 ball')
+.setDescription('**Util Commands**\n\n**sb;serverinfo** - Shows info about the server.\n**sb;userinfo** - Shows info about you or another person\n**sb;botinfo** - Info about me\n**sb;kick** - Kick a user\n\n**Role commands**\n\n**sb;role** - Custom roles. Role name must start with • and be below the bots role\n**sb;color** - Colored roles. Role name must start with # and be below the the bots role\n\n**Fun Commands**\n\n**sb;cat** - Random cat (has api cooldown)\n**sb;gay** - Call someone gay\n**sb;retarded** - Call someone retarded\n**sb;say** - Make me say stuff\n**sb;yomama** - Yo mama joke\n**sb;8ball** - Magic 8 ball')
 .setFooter('The help command is being redone and all commands may not be shown.');
 message.author.send({embed: help});
 message.channel.send('📬 Dms.');
