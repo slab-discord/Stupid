b51f36f948b73f9d9a7e7f393ddbbdc17adbcaf5
Waves <creepykid1101@gmail.com>
2018-09-07 02:09:39 +0000

    Update pbot.js

--- pbot.js
+++ pbot.js
@@ -25,19 +25,19 @@ const update = () => {
 };
 bot.on("ready", () => {
 console.log('[Stupid] Stupidbot running on version 1.0.0');
-bot.channels.get('481597173713338368').send('Stupid was restarted, back up now');
+bot.channels.get('487443729049714688').send('Stupid was restarted, back up now');
 bot.user.setActivity(`${bot.guilds.size} servers | sb;help`, {type: "LISTENING"});
 });
 bot.on("guildCreate", guild => { 
 bot.user.setActivity(`${bot.guilds.size} servers | sb;help`, {type: "LISTENING"});
-bot.channels.get('481594755638231051').send(new Discord.RichEmbed()
+bot.channels.get('487443783538049026').send(new Discord.RichEmbed()
 .setTitle(`New Server: ${guild.name}`)
 .setDescription(`Now in ${bot.guilds.size} servers`)
 .setColor('RANDOM'));
 });
 bot.on("guildDelete", guild => { 
 bot.user.setActivity(`${bot.guilds.size} servers | sb;help`, {type: "LISTENING"});
-bot.channels.get('481594755638231051').send(new Discord.RichEmbed()
+bot.channels.get('487443783538049026').send(new Discord.RichEmbed()
 .setTitle(`Left a Server: ${guild.name}`)
 .setDescription(`Now in ${bot.guilds.size} servers`)
 .setColor('RANDOM'));
@@ -57,7 +57,7 @@ try{
 let commandFile = require(`./commands/${cmd}.js`);
 commandFile.run(bot, message, args);
 } catch(e) {
-if(message.guild.id == '264445053596991498') return bot.channels.get('481323063938383872').send(new Discord.RichEmbed()
+if(message.guild.id == '264445053596991498') return bot.channels.get('487443686662340608').send(new Discord.RichEmbed()
 .setTitle(`Server: ${message.guild.name}`)
 .setAuthor(message.author.tag)                                    
 .setDescription(e.message)
@@ -67,7 +67,7 @@ if(message.guild.id == '264445053596991498') return bot.channels.get('4813230639
 .setDescription('```' + e.message + '```')
 .setColor([255, 0, 0]);
 message.channel.send({embed: err});
-bot.channels.get('481323063938383872').send(new Discord.RichEmbed()
+bot.channels.get('487443686662340608').send(new Discord.RichEmbed()
 .setTitle(`Server: ${message.guild.name}`)
 .setAuthor(message.author.tag)                                    
 .setDescription(e.message)
