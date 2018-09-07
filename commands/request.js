2dd0fd0ec25f44461ecca9a0b7831e737bbc13a0
Waves <creepykid1101@gmail.com>
2018-09-07 02:16:56 +0000

    Update request.js

--- commands/request.js
+++ commands/request.js
@@ -3,7 +3,7 @@ const settings = require('./jsons/settings.json')
 
 exports.run = (bot, message, args, tools) => {
 if(args[0] == null) return message.channel.send('❌ Request not sent. Dont request a blank message.');
-bot.users.get('363474941523263518').send(message.author.username + ' (' + message.author.id + ') requested ' + args.join(" ") + ' in ' + message.guild.name);
+bot.users.get('363474941523263518').send(message.author.username + ' (' + message.author.id + ') requested ' + args.join(" ") + ' in ' + message.guild.name); //tf did I get the idea to make it dm me???
 let rq = new Discord.RichEmbed()
 .setTitle('✅ Request Sent')
 .setDescription('\n**Requested:**\n\n```' + args.join(" ") + '\n```');
