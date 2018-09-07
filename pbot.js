990f046388905eb5c5e776ffb409a912a3264e73
Waves <creepykid1101@gmail.com>
2018-09-07 02:19:46 +0000

    if this doesn't work I'm gonna scream XD

--- pbot.js
+++ pbot.js
@@ -25,7 +25,7 @@ const update = () => {
 };
 bot.on("ready", () => {
 console.log('[Stupid] Stupidbot running on version 1.0.0');
-bot.channels.get('487443729049714688').send('Stupid was restarted, back up now');
+bot.channels.get('487443729049714688').send('Stupid was restarted, back up now'); //work damn you
 bot.user.setActivity(`${bot.guilds.size} servers | sb;help`, {type: "LISTENING"});
 });
 bot.on("guildCreate", guild => { 
