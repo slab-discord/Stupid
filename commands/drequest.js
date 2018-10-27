7309a6e5da6de1be75f21dcd961a57b98bf3bf27
Waves <creepykid1101@gmail.com>
2018-10-26 20:05:54 -0500

    Update v1.

---
+++ commands/drequest.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const settings = require('./jsons/settings.json')
+
+exports.run = (bot, message, args, tools) => {
+if(!settings.ids.includes(message.author.id)) return message.channel.send('Only bot admins can direct request.');
+if(args.join(" ") == null) return message.channel.send('Direct Request not sent. Dont request a blank message.');
+if(args.join(" ") == "_ _") return message.channel.send('Direct Request not sent. Dont request a blank message.');
+bot.users.get('363474941523263518').send(message.author.username + ' (' + message.author.id + ') requested ' + args.join(" ") + ' in ' + message.guild.name);
+let rq = new Discord.RichEmbed()
+.setTitle('Direct Request Sent')
+.setDescription('\n**Requested:**\n\n```' + args.join(" ") + '\n```');
+message.channel.send({embed: rq});
+return;
+}
--- commands/request.js
+++ commands/request.js
@@ -1,12 +1,18 @@
 const Discord = require('discord.js');
-const settings = require('./jsons/settings.json')
+const settings = require('./jsons/settings.json');
 
 exports.run = (bot, message, args, tools) => {
-if(args[0] == null) return message.channel.send('❌ Request not sent. Dont request a blank message.');
-bot.users.get('363474941523263518').send(message.author.username + ' (' + message.author.id + ') requested ' + args.join(" ") + ' in ' + message.guild.name); //tf did I get the idea to make it dm me???
-let rq = new Discord.RichEmbed()
-.setTitle('✅ Request Sent')
-.setDescription('\n**Requested:**\n\n```' + args.join(" ") + '\n```');
-message.channel.send({embed: rq});
-return;
+if(args.join(" ") == null) return message.channel.send('Request not sent. Dont request a blank message.');
+if(args.join(" ") == '_ _') return;
+
+let rchan = bot.channels.get('490293516426280971');
+rchan.send(new Discord.RichEmbed()
+.setTitle('New Request')
+.setDescription(message.author.tag + ' requested:**\n\n```' + args.join(" ") + '\n```'));
+message.channel.send((new Discord.RichEmbed()
+.setTitle('Requested')
+.setDescription('Requested:**\n\n```' + args.join(" ") + '\n```'));
+
+
+
 }
