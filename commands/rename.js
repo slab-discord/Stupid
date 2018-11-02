8281cf132ad48fa82b9a31dc7bc94bf341b37571
Waves <creepykid1101@gmail.com>
2018-11-02 12:53:03 -0500

    Made it better ig

--- commands/rename.js
+++ commands/rename.js
@@ -4,5 +4,5 @@ let settings = require('./jsons/settings.json');
 exports.run = (bot, message, args, tools) => {
 if(message.author.id !== settings.myid) return;
 bot.user.setUsername(args.join(" "));
-message.channel.send('I have been renamed!');
+message.channel.send(`I have been renamed to: ${args.join(" ")});
 } 
