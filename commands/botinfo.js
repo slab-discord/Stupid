ab418e37dba63caced54a3127c100b0f753e66e2
Waves <creepykid1101@gmail.com>
2018-11-02 15:39:32 +0000

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -10,7 +10,7 @@ const moment = require('moment');
 const duration = moment.duration(bot.uptime).format(' D [days], H [hrs], m [mins], s [secs]');
 let binfo = new Discord.RichEmbed()
 .setAuthor(bot.user.username, bot.user.displayAvatarURL)
-.setDescription(`**Uptime:** ${duration}\n**CPU:** ${usdcpu} | **Ram:** ${(used).toFixed(2)} MB\n${bot.guilds.size} **Servers** | ${bot.users.size} **Users**\n**Library:** [discord.js](https:/\/discord.js.org/#/) **Version:** ${Discord.version}\n**Developer: Wayve#6498**\n[Invite Me](https:/\/discordapp.com/api/oauth2/authorize?client_id=405635474124832768&permissions=8&scope=bot) | [DBL](https:/\/discordbots.org/bot/405635474124832768) | [Trash](https:/\/discord.gg/5mpFZkU)`)
+.setDescription(`**Uptime:** ${duration}\n**CPU:** ${usdcpu} | **Ram:** ${(used).toFixed(2)} MB\n${bot.guilds.size} **Servers** | ${bot.users.size} **Users**\n**Library:** [discord.js](https:/\/discord.js.org/#/) **Version:** ${Discord.version}\n**Developer: ${bot.users.get(settings.myid).tag}**\n[Invite Me](https:/\/discordapp.com/api/oauth2/authorize?client_id=405635474124832768&permissions=8&scope=bot) | [DBL](https:/\/discordbots.org/bot/405635474124832768) | [Trash](https:/\/discord.gg/5mpFZkU)`)
 .setThumbnail(bot.user.displayAvatarURL)
 .setFooter('Stupid Bot v' + settings.version)
 .setColor('#f44242');
