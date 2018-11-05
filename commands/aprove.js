5ef77c73fb8880f7cec2bf7e1f25d7236dee6f5f
Waves <creepykid1101@gmail.com>
2018-11-05 13:16:56 -0600

    Update aprove.js

--- commands/aprove.js
+++ commands/aprove.js
@@ -4,6 +4,7 @@ const settings = require('./jsons/settings.json');
 exports.run = (bot, message, args, tools) => {
 if(message.channel.id !== '480300841123708939') return;
 if(isNaN(args.join(" ").slice(0, 18))) return message.channel.send('Please enter the bots id.');
+if(!args.join(" ").slice(19)) return message.channel.send('Please enter the users id.');
 if(isNaN(args.join(" ").slice(19))) return message.channel.send('Please enter the users id.');
 
 message.channel.send('Bot Approved.')
