d6af5f7ac0047ba727e91682d493d5f1a7013653
Waves <creepykid1101@gmail.com>
2018-11-08 13:08:07 -0600

    Delete retarded.js

--- commands/retarded.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const settings = require('./jsons/settings.json');
-
-exports.run = (bot, message, args, tools) => {
-let retard = message.mentions.users.first();
-if(!retard) return message.channel.send(message.guild.members.random().user.username + ' is retarded');
-message.delete();
-message.channel.send(retard + ' ur retarded');
-}
