f8ee3bf328facb2f861eb7dde89e657fcf5c21bb
Waves <creepykid1101@gmail.com>
2018-11-08 13:08:17 -0600

    Delete gay.js

--- commands/gay.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const settings = require('./jsons/settings.json');
-
-exports.run = (bot, message, args, tools) => {
-let gay = message.mentions.users.first();
-if(!gay) return message.channel.send(message.guild.members.random().user.username + ' is gay');
-message.delete();
-message.channel.send(gay + ' ur gay');
-}
